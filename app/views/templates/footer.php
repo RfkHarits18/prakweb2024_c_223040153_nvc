c2cc88adbdf7899af86eacfaf7505c5943107916
Rifqi Harits <rharits18@gmail.com>
2024-10-28 19:06:17 +0700

    first commit

---
+++ app/views/templates/footer.php
@@ -0,0 +1,6 @@
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo"crossorigin="anonymous"></script>
+<script scr="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBkOWLaUAdn689aCwoqbBJiSnjAK/L8WvCWPIPm49" crossorigin="anoymous"></script>
+<script scr="<?= BASEURL; ?>/js/bootstrap."></script>
+<script scr="<?= BASEURL; ?>/js/script.js."></script>
+</body>
+</html>
